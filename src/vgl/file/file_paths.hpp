728d1c6c39ed426e480f2318ffe3572f70068ea9
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2019-04-27 15:12:26 +0200

    Remove generated file_paths.hpp

--- src/vgl/file/file_paths.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace vgl
-{
-    const std::filesystem::path resources_path = "C:/Users/alexa/Documents/projects/vgl/resources/";
-    const std::filesystem::path shaders_path = "C:/Users/alexa/Documents/projects/vgl/build/x64-Debug/shaders/";
-}
