eaff32fada2681d49c53bdbfa85b7b905999c52b
Alexander Scheid-Rehder <alexanderb@scheid-rehder.de>
2019-04-30 09:35:33 +0200

    Implement WIP of separated filters demo

---
+++ projects/example_separated_filters/main.cpp
@@ -0,0 +1,94 @@
+#include "GLFW/glfw3.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+#include "glm/glm.hpp"
+#include "vgl/file/file.hpp"
+#include "vgl/gpu_api/gl/shader.hpp"
+#include "vgl/gpu_api/gl/buffer.hpp"
+#include "vgl/gpu_api/gl/vao.hpp"
+#include "vgl/gpu_api/gl/texture.hpp"
+#include "vgl/gpu_api/gl/fbo.hpp"
+#include "imgui/imgui.h"
+#include "imgui/examples/imgui_impl_glfw.h"
+#include "imgui/examples/imgui_impl_opengl3.h"
+
+int main() {
+    glfwInit();
+    auto w_res = glm::ivec2(1600, 900);
+    auto window = glfwCreateWindow(w_res.x, w_res.y, "Hello", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glViewport(0, 0, w_res.x, w_res.y);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 460");
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    auto vertex_shader = vgl::gl::create_shader_spirv(GL_VERTEX_SHADER, vgl::shaders_path / "minimal/texture.vert");
+    auto frag_shader = vgl::gl::create_shader_spirv(GL_FRAGMENT_SHADER, vgl::shaders_path / "filters/separated_first_pass.frag");
+
+    const auto first_pass = vgl::gl::create_program(vertex_shader, frag_shader);
+
+    vgl::gl::delete_shaders(vertex_shader, frag_shader);
+    
+    GLuint tex_id = vgl::gl::create_texture(GL_TEXTURE_2D);
+    glTextureParameteri(tex_id, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTextureParameteri(tex_id, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTextureParameteri(tex_id, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTextureParameteri(tex_id, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+
+    glm::ivec2 image_size{};
+    auto image_path = (vgl::resources_path / "images/naturo-monkey-selfie.jpg").string();
+    stbi_info(image_path.c_str(), &image_size.x, &image_size.y, nullptr);
+
+    stbi_set_flip_vertically_on_load(1);
+    auto image_channels = 4;
+    auto ptr = std::unique_ptr<stbi_uc>(stbi_load(image_path.c_str(), &image_size.x, &image_size.y,
+                                                  &image_channels, image_channels));
+
+    glTextureStorage2D(tex_id, 1, GL_RGBA8, image_size.x, image_size.y);
+    glTextureSubImage2D(tex_id, 0, 0, 0, image_size.x, image_size.y, GL_RGBA, GL_UNSIGNED_BYTE, ptr.get());
+    glBindTextureUnit(0, tex_id);
+
+    GLuint framebuffer = vgl::gl::create_framebuffer();
+
+    glUseProgram(first_pass);
+
+    auto screen_vao = vgl::gl::create_vertex_array();
+
+    int kernel_size = 0;
+    int filter = 0;
+    float strength = 0.0f;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        if (ImGui::Begin("Kernel size")) {
+            ImGui::SliderInt("Filter", &filter, 0, 100);
+            ImGui::SliderInt("Kernel Size", &kernel_size, 0, 100);
+            ImGui::DragFloat("Strength", &strength, 0.01f, 0.0f, 100.0f);
+            glProgramUniform1i(first_pass, 0, kernel_size);
+            glProgramUniform1i(first_pass, 1, filter);
+            glProgramUniform1i(first_pass, 2, strength);
+        }
+        ImGui::End();
+        glBindVertexArray(screen_vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
---
+++ shaders/filters/separated_second_pass.frag
@@ -0,0 +1,81 @@
+#version 460
+
+layout (location = 0) in vec2 _uv;
+
+layout (binding = 0) uniform sampler2D og_tex;
+layout (binding = 1) uniform sampler2D tex;
+
+layout (location = 0) uniform int kernel_size;
+layout (location = 1) uniform int filter_option;
+layout (location = 2) uniform float strength;
+
+layout (location = 0) out vec4 _color;
+
+float factorial(float n) {
+    float result = 1;
+    for (float i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+float binomial_denom(float n, float k) {
+    return factorial(k) * factorial(n - k);
+}
+
+#define PI 3.1415926535897932384626433832795
+
+void main() {
+    ivec2 tex_size = textureSize(tex, 0);
+    vec2 uv_offset = vec2(1, 1) / vec2(tex_size);
+	float half_size = (kernel_size - 1) / 2.0;
+	if (kernel_size <= 0) {
+        _color = texture(tex, _uv);
+		return;
+	}
+
+    // box blur 1 or unsharp masking 2
+    if ((filter_option == 1)) {
+        vec3 mean = vec3(0.0);
+        for (float i = -half_size; i <= half_size; i++) {
+            mean += texture(tex, _uv + uv_offset * vec2(0, i)).rgb;
+        }
+        _color.rgb = mean / kernel_size / kernel_size;
+		_color.a = 1.0f;
+    }
+    // binomial filter
+    else if (filter_option == 2) {
+        float n = kernel_size - 1.0;
+        float n_factorial = factorial(n);
+        vec3 mean = vec3(0.0);
+        for (int i = 0; i < kernel_size; i++) {
+            mean += n_factorial / binomial_denom(n, i)
+                    * texture(tex, _uv + uv_offset * vec2(0, i - half_size)).rgb;
+        }
+		float factor = pow(2.0, float(n));
+        _color.rgb = mean / factor;
+		_color.a = 1.0f;
+    }
+	// Gaussian blur
+    else if (filter_option == 3 || filter_option == 4) {
+		float sigma = kernel_size / 6.0;
+		float sigma2 = 2.0 * sigma * sigma;
+		float denom = 1.0 / sqrt(PI * sigma2);
+        vec3 mean = vec3(0.0);
+		float gauss_sum = 0.0;
+        for (float i = -half_size; i <= half_size; i++) {
+			float gauss = denom * exp(-(i * i) / sigma2);
+            mean += gauss * texture(tex, _uv + uv_offset * vec2(0, i)).rgb;
+			gauss_sum += gauss;
+        }
+        _color.rgb = mean / gauss_sum;
+		if (filter_option == 4) {
+			vec3 og = texture(og_tex, _uv).rgb;
+			_color.rgb = og + strength * (og - _color.rgb);
+		}
+		_color.a = 1.0f;
+    }
+    else {
+        _color = texture(tex, _uv);
+    }
+}
