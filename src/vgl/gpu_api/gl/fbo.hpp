b7f4b5d2a70f4b01b99a66944f758a0d6aadd4a1
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2019-05-01 13:45:21 +0200

    Separate fbo functions

--- src/vgl/gpu_api/gl/fbo.hpp
+++ src/vgl/gpu_api/gl/fbo.hpp
@@ -1,17 +1,22 @@
 #pragma once
 
 namespace vgl::gl {
-    template <typename... Args>
-    GLuint create_framebuffer(Args&&... args) {
-        GLuint framebuffer = 0;
-        glCreateFramebuffers(1, &framebuffer);
-        if constexpr (sizeof...(args) != 0) {
-            std::vector<GLenum> attachments{ std::forward<Args>(args)... };
-            glNamedFramebufferDrawBuffers(framebuffer, static_cast<GLsizei>(attachments.size()), attachments.data());
-        }
+
+    void check_framebuffer(GLuint framebuffer) {
         if (glCheckNamedFramebufferStatus(framebuffer, GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
             throw std::runtime_error{ "Framebuffer " + std::to_string(framebuffer) + " was not created." };
         }
+    }
+    GLuint create_framebuffer() {
+        GLuint framebuffer = 0;
+        glCreateFramebuffers(1, &framebuffer);
         return framebuffer;
+    }
+    template <typename... Args>
+    void attach_drawbuffers(GLuint framebuffer, Args... args) {
+        if constexpr (sizeof...(args) != 0) {
+            std::vector<GLenum> attachments{static_cast<GLenum>(std::forward<Args>(args))... };
+            glNamedFramebufferDrawBuffers(framebuffer, attachments.size(), attachments.data());
+        }
     }
 }
