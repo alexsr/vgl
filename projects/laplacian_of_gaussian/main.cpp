c046a768836f3697a2bad89cca18197633cfb492
Alexander Scheid-Rehder <alexanderb@scheid-rehder.de>
2019-05-02 17:34:56 +0200

    Add monochrome filter to laplacian demo

--- projects/laplacian_of_gaussian/main.cpp
+++ projects/laplacian_of_gaussian/main.cpp
@@ -33,15 +33,17 @@ int main() {
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
     auto vertex_shader = vgl::gl::create_shader_spirv(GL_VERTEX_SHADER, vgl::shaders_path / "minimal/texture.vert");
+    auto monochrome_shader = vgl::gl::create_shader_spirv(GL_FRAGMENT_SHADER, vgl::shaders_path / "filters/monochrome.frag");
     auto gauss_x_shader = vgl::gl::create_shader_spirv(GL_FRAGMENT_SHADER, vgl::shaders_path / "filters/gauss_x.frag");
     auto gauss_y_shader = vgl::gl::create_shader_spirv(GL_FRAGMENT_SHADER, vgl::shaders_path / "filters/gauss_y.frag");
-    auto laplacian_shader = vgl::gl::create_shader_spirv(GL_FRAGMENT_SHADER, vgl::shaders_path / "filters/laplacian.frag");
+    auto laplacian_shader = vgl::gl::create_shader_spirv(GL_FRAGMENT_SHADER, vgl::shaders_path / "blob_detection/laplacian_normalized.frag");
 
+    const auto monochrome = vgl::gl::create_program(vertex_shader, monochrome_shader);
     const auto gauss_x = vgl::gl::create_program(vertex_shader, gauss_x_shader);
     const auto gauss_y = vgl::gl::create_program(vertex_shader, gauss_y_shader);
     const auto laplacian = vgl::gl::create_program(vertex_shader, laplacian_shader);
 
-    vgl::gl::delete_shaders(vertex_shader, gauss_x_shader, gauss_y_shader, laplacian_shader);
+    vgl::gl::delete_shaders(vertex_shader, monochrome_shader, gauss_x_shader, gauss_y_shader, laplacian_shader);
 
     GLuint tex_id = vgl::gl::create_texture(GL_TEXTURE_2D);
     glTextureParameteri(tex_id, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -97,25 +99,30 @@ int main() {
             ImGui::SliderInt("Kernel Size", &kernel_size, 0, 100);
             glProgramUniform1i(gauss_x, 0, kernel_size);
             glProgramUniform1i(gauss_y, 0, kernel_size);
+            glProgramUniform1i(laplacian, 0, kernel_size);
         }
         ImGui::End();
-        glUseProgram(gauss_x);
+        glUseProgram(monochrome);
         glBindTextureUnit(0, tex_id);
         glViewport(0, 0, image_size.x, image_size.y);
-        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_gauss_x);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_gauss_y);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glBindVertexArray(screen_vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glUseProgram(gauss_x);
+        glBindTextureUnit(0, gauss_y_tex);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_gauss_x);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glUseProgram(gauss_y);
         glBindTextureUnit(0, gauss_x_tex);
         glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_gauss_y);
-        glBindVertexArray(screen_vao);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glUseProgram(laplacian);
         glBindTextureUnit(0, gauss_y_tex);
         glViewport(0, 0, w_res.x, w_res.y);
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        glBindVertexArray(screen_vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
--- shaders/blob_detection/laplacian_normalized.frag
+++ shaders/blob_detection/laplacian_normalized.frag
@@ -4,16 +4,20 @@ layout (location = 0) in vec2 _uv;
 
 layout (binding = 0) uniform sampler2D tex;
 
+layout (location = 0) uniform int kernel_size;
+
 layout (location = 0) out vec4 _color;
 
 void main() {
     ivec2 tex_size = textureSize(tex, 0);
     vec2 uv_offset = vec2(1, 1) / vec2(tex_size);
+    float sigma = kernel_size / 6.0;
+    float sigma2 = 2.0 * sigma * sigma;
     vec4 left = texture(tex, _uv + uv_offset * vec2(-1, 0));
     vec4 top = texture(tex, _uv + uv_offset * vec2(0, 1));
     vec4 mid = texture(tex, _uv);
     vec4 right = texture(tex, _uv + uv_offset * vec2(1, 0));
     vec4 bottom = texture(tex, _uv + uv_offset * vec2(0, -1));
     _color = left + top - 4.0 * mid + right + bottom;
-    _color = _color * 10;
+    _color = _color * sigma2;
 }
