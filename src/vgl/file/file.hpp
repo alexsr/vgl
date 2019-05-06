839de623b4c09fd32af43abadfa8b31619758a89
Alexander Scheid-Rehder <alexsr@uni-koblenz.de>
2019-05-06 04:31:37 +0200

    Add async file loading function

--- src/vgl/file/file.hpp
+++ src/vgl/file/file.hpp
@@ -4,22 +4,29 @@
 #include <optional>
 #include "file_paths.hpp"
 #include <fstream>
+#include <future>
 
-namespace vgl {
-	std::optional<std::filesystem::path> open_file_dialog(std::filesystem::path default_path, const std::string& filter = "") {
-		nfdchar_t* res = nullptr;
-		auto status = NFD_OpenDialog(filter.c_str(), default_path.make_preferred().string().c_str(), &res);
-		if (status == NFD_CANCEL || status == NFD_ERROR) {
-			return std::nullopt;
-		}
-		return res;
-	}
+namespace vgl
+{
+    std::optional<std::filesystem::path> open_file_dialog(std::filesystem::path default_path,
+        const std::string& filter = "") {
+        nfdchar_t* res = nullptr;
+        auto status = NFD_OpenDialog(filter.c_str(), default_path.make_preferred().string().c_str(), &res);
+        if (status == NFD_CANCEL || status == NFD_ERROR) {
+            return std::nullopt;
+        }
+        return res;
+    }
 
-	std::vector<std::byte> load_file_binary(const std::filesystem::path& file_path) {
-		std::ifstream f(file_path, std::ios::binary | std::ios::in);
-		const auto file_size = std::filesystem::file_size(file_path);
-		std::vector<std::byte> buffer(file_size);
-		f.read(reinterpret_cast<char*>(buffer.data()), file_size);
-		return buffer;
-	}
+    std::vector<std::byte> load_binary_file(const std::filesystem::path& file_path) {
+        std::ifstream f(file_path, std::ios::binary | std::ios::in);
+        const auto file_size = std::filesystem::file_size(file_path);
+        std::vector<std::byte> buffer(file_size);
+        f.read(reinterpret_cast<char*>(buffer.data()), file_size);
+        return buffer;
+    }
+
+    std::future<std::vector<std::byte>> load_binary_file_async(std::filesystem::path file_path) {
+        return std::async(std::launch::async, [f = std::move(file_path)]() { return load_binary_file(f); });
+    }
 }
