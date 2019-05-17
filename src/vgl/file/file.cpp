d0418c07c713be958cd9c794ec7278f8865f5e70
Alexander Scheid-Rehder <alexanderb@scheid-rehder.de>
2019-05-18 00:20:38 +0200

    Move file impl to file cpp

---
+++ src/vgl/file/file.cpp
@@ -0,0 +1,60 @@
+#include "file.hpp"
+#include "nfd.h"
+#include <fstream>
+
+bool vgl::file::is_file(const std::filesystem::path& path) {
+    return exists(path) && !is_directory(path);
+}
+
+bool vgl::file::is_file(const char* path) {
+    auto temp = std::filesystem::path(path);
+    return exists(temp) && !is_directory(temp);
+}
+
+std::optional<std::filesystem::path> vgl::file::open_file_dialog(std::filesystem::path default_path,
+    const std::string& filter) {
+    nfdchar_t* res = nullptr;
+    auto status = NFD_OpenDialog(filter.c_str(), default_path.make_preferred().string().c_str(), &res);
+    if (status == NFD_CANCEL || status == NFD_ERROR) {
+        return std::nullopt;
+    }
+    return res;
+}
+
+std::optional<std::vector<std::filesystem::path>> vgl::file::open_multiple_files_dialog(std::filesystem::path default_path,
+    const std::string& filter) {
+    nfdpathset_t res;
+    auto status = NFD_OpenDialogMultiple(filter.c_str(), default_path.make_preferred().string().c_str(), &res);
+    if (status == NFD_CANCEL || status == NFD_ERROR) {
+        return std::nullopt;
+    }
+    std::vector<std::filesystem::path> paths(res.count);
+    for (int i = 0; i < res.count; ++i) {
+        paths.at(i) = res.buf + res.indices[i];
+    }
+    return paths;
+}
+
+std::string vgl::file::load_string_file(const std::filesystem::path& file_path) {
+    std::ifstream f(file_path, std::ios::binary | std::ios::in);
+    const auto file_size = std::filesystem::file_size(file_path);
+    std::string buffer(file_size, ' ');
+    f.read(reinterpret_cast<char*>(buffer.data()), file_size);
+    return buffer;
+}
+
+std::future<std::string> vgl::file::load_string_file_async(std::filesystem::path file_path) {
+    return std::async(std::launch::async, [f = std::move(file_path)]() { return load_string_file(f); });
+}
+
+std::vector<std::byte> vgl::file::load_binary_file(const std::filesystem::path& file_path) {
+    std::ifstream f(file_path, std::ios::binary | std::ios::in);
+    const auto file_size = std::filesystem::file_size(file_path);
+    std::vector<std::byte> buffer(file_size);
+    f.read(reinterpret_cast<char*>(buffer.data()), file_size);
+    return buffer;
+}
+
+std::future<std::vector<std::byte>> vgl::file::load_binary_file_async(std::filesystem::path file_path) {
+    return std::async(std::launch::async, [f = std::move(file_path)]() { return load_binary_file(f); });
+}
--- src/vgl/file/file.hpp
+++ src/vgl/file/file.hpp
@@ -1,67 +1,19 @@
 #pragma once
 
-#include "nfd.h"
 #include <optional>
 #include "file_paths.hpp"
-#include <fstream>
 #include <future>
 
 namespace vgl::file
 {
-    inline bool is_file(const std::filesystem::path& path) {
-        return exists(path) && !is_directory(path);
-    }
-
-    inline bool is_file(const char* path) {
-        auto temp = std::filesystem::path(path);
-        return exists(temp) && !is_directory(temp);
-    }
-
-    inline std::optional<std::filesystem::path> open_file_dialog(std::filesystem::path default_path,
-        const std::string& filter = "") {
-        nfdchar_t* res = nullptr;
-        auto status = NFD_OpenDialog(filter.c_str(), default_path.make_preferred().string().c_str(), &res);
-        if (status == NFD_CANCEL || status == NFD_ERROR) {
-            return std::nullopt;
-        }
-        return res;
-    }
-
-    inline std::optional<std::vector<std::filesystem::path>> open_multiple_files_dialog(std::filesystem::path default_path,
-        const std::string& filter = "") {
-        nfdpathset_t res;
-        auto status = NFD_OpenDialogMultiple(filter.c_str(), default_path.make_preferred().string().c_str(), &res);
-        if (status == NFD_CANCEL || status == NFD_ERROR) {
-            return std::nullopt;
-        }
-        std::vector<std::filesystem::path> paths(res.count);
-        for (int i = 0; i < res.count; ++i) {
-            paths.at(i) = res.buf + res.indices[i];
-        }
-        return paths;
-    }
-
-    inline std::string load_string_file(const std::filesystem::path& file_path) {
-        std::ifstream f(file_path, std::ios::binary | std::ios::in);
-        const auto file_size = std::filesystem::file_size(file_path);
-        std::string buffer(file_size, ' ');
-        f.read(reinterpret_cast<char*>(buffer.data()), file_size);
-        return buffer;
-    }
-
-    inline std::future<std::string> load_string_file_async(std::filesystem::path file_path) {
-        return std::async(std::launch::async, [f = std::move(file_path)]() { return load_string_file(f); });
-    }
-
-    inline std::vector<std::byte> load_binary_file(const std::filesystem::path& file_path) {
-        std::ifstream f(file_path, std::ios::binary | std::ios::in);
-        const auto file_size = std::filesystem::file_size(file_path);
-        std::vector<std::byte> buffer(file_size);
-        f.read(reinterpret_cast<char*>(buffer.data()), file_size);
-        return buffer;
-    }
-
-    inline std::future<std::vector<std::byte>> load_binary_file_async(std::filesystem::path file_path) {
-        return std::async(std::launch::async, [f = std::move(file_path)]() { return load_binary_file(f); });
-    }
+    bool is_file(const std::filesystem::path& path);
+    bool is_file(const char* path);
+    std::optional<std::filesystem::path> open_file_dialog(std::filesystem::path default_path,
+        const std::string& filter = "");
+    std::optional<std::vector<std::filesystem::path>> open_multiple_files_dialog(std::filesystem::path default_path,
+        const std::string& filter = "");
+    std::string load_string_file(const std::filesystem::path& file_path);
+    std::future<std::string> load_string_file_async(std::filesystem::path file_path);
+    std::vector<std::byte> load_binary_file(const std::filesystem::path& file_path);
+    std::future<std::vector<std::byte>> load_binary_file_async(std::filesystem::path file_path);
 }
