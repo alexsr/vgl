88ae6fbff4e22984d8efbfe54cd2e310a8100a5f
Alexander Scheid-Rehder <alexanderb@scheid-rehder.de>
2019-05-17 21:37:16 +0200

    Add buffer update methods

--- src/vgl/gpu_api/gl/buffer.hpp
+++ src/vgl/gpu_api/gl/buffer.hpp
@@ -7,17 +7,33 @@
 namespace vgl::gl
 {
     template <typename T>
-    std::enable_if_t<is_container_v<T>> set_buffer_storage(const GLuint buffer, T data, GLenum bitfield = 0) {
+    std::enable_if_t<is_container_v<T>> set_buffer_storage(const glbuffer& buffer, const T& data, GLenum bitfield = 0) {
         glNamedBufferStorage(buffer, std::size(data) * sizeof_value_type(data), std::data(data), bitfield);
     }
 
     template <typename T>
-    std::enable_if_t<!is_container_v<T>> set_buffer_storage(const GLuint buffer, T data, GLenum bitfield = 0) {
-        glNamedBufferStorage(buffer, sizeof data, &data, bitfield);
+    std::enable_if_t<!is_container_v<T>> set_buffer_storage(const glbuffer& buffer, const T& data, GLenum bitfield = 0) {
+        glNamedBufferStorage(buffer, sizeof(T), &data, bitfield);
     }
 
-    void set_empty_buffer_storage(const GLuint buffer, size_t size, GLenum bitfield = 0) {
+    void set_empty_buffer_storage(const glbuffer& buffer, size_t size, GLenum bitfield = 0) {
         glNamedBufferStorage(buffer, size, nullptr, bitfield);
+    }
+
+    template <typename T>
+    std::enable_if_t<is_container_v<T>> update_full_buffer(const glbuffer& buffer, const T& data) {
+        const auto buffer_ptr = glMapNamedBufferRange(buffer, 0, std::size(data) * sizeof_value_type(data),
+            GL_MAP_INVALIDATE_BUFFER_BIT | GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_RANGE_BIT);
+        std::memcpy(buffer_ptr, data.data(), std::size(data) * sizeof_value_type(data));
+        glUnmapNamedBuffer(buffer);
+    }
+
+    template <typename T>
+    std::enable_if_t<!is_container_v<T>> update_full_buffer(const glbuffer& buffer, const T& data) {
+        const auto buffer_ptr = glMapNamedBufferRange(buffer, 0, sizeof(T),
+            GL_MAP_INVALIDATE_BUFFER_BIT | GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_RANGE_BIT);
+        std::memcpy(buffer_ptr, &data, sizeof(T));
+        glUnmapNamedBuffer(buffer);
     }
 
     template <typename T>
